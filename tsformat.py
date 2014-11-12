e98c3da0b845c7f4e4dd94f8b1f7614e10b0adc8
zhou_wei <secret32@sina.com>
2014-11-12 14:05:44 +0800

    add: tsformat.py

---
+++ tsformat.py
@@ -0,0 +1,14 @@
+import time, sys, os
+
+def formatTimeStamp(ts):
+    lt = time.localtime(ts)
+    t = time.strftime("%Y-%m-%d %H:%M:%S", lt)
+    return t
+
+ts = 0
+if len(sys.argv) >= 2:
+    ts = int(sys.argv[1])
+else:
+    ts = time.time()
+print(formatTimeStamp(ts))
+
